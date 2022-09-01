96533f7b23ca6f42f9325980f7d9f352106ca02d
javierjcf <javier@comunitea.com>
2022-09-01 17:45:11 +0200

    Vim/Kittie config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,8 @@ require('keymaps')
 require('plugins')
 
 -- cargo el esquema de color en la carpeta plugin
+vim.cmd("colorscheme nordfox")
+
 
 require("nvim-tree").setup()
 require("lualine").setup()
