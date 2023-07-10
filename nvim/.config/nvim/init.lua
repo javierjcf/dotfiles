1c39e48272e6df7f8052cc775e0cf4636259b05e
javierjcf <javier@comunitea.com>
2023-07-10 16:57:27 +0200

    [ADD]neovim: Config Lazy packer manager

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,25 +1,4 @@
 require('settings')
 require('keymaps')
-require('plugins')
+require('lazy-config')
 
--- cargo el esquema de color en la carpeta plugin
-vim.cmd("colorscheme nordfox")
-
-
-require("nvim-tree").setup()
-require("lualine").setup()
-require('telescope').setup{
-  defaults = {
-    vimgrep_arguments = {
-      'rg',
-      '--color=never',
-      '--no-heading',
-      '--with-filename',
-      '--line-number',
-      '--column',
-      '--smart-case',
-      '--no-ignore',
-      -- :wq'-u' -- Para que no respete gitignore
-    },
-  }
-}
