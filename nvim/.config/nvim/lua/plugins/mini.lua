60d164787b5262a5168d9be4e368bc950cabbce1
javierjcf <javier@comunitea.com>
2023-07-17 19:40:02 +0200

    [ADD]neovim: Autopairs, comment

---
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,20 @@
+return {
+    {
+      "echasnovski/mini.pairs",
+      event = "VeryLazy",
+      opts = {},
+    },
+    {
+      "echasnovski/mini.comment",
+      event = "VeryLazy",
+      dependencies = {'JoosepAlviste/nvim-ts-context-commentstring'},
+      opts = {
+        options = {
+          custom_commentstring = function()
+            return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+          end,
+        },
+      },
+    }
+}
+
