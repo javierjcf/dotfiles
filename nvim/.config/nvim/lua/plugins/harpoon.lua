5a2a9c4447f965e93eba8e1af76b256ce44995eb
javierjcf <javier@comunitea.com>
2023-07-18 02:00:56 +0200

    [ADD]nvim: Harpoon

---
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,11 @@
+return {
+  "ThePrimeagen/harpoon",
+  keys = {
+    { "<leader>a", function() require("harpoon.mark").add_file() end, desc = "Add file Harpoon" },
+    { "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Open Harpoon menu" },
+    { "<leader>1", function() require("harpoon.ui").nav_file(1) end, desc = "Nav file 1" },
+    { "<leader>2", function() require("harpoon.ui").nav_file(2) end, desc = "Nav file 2" },
+    { "<leader>3", function() require("harpoon.ui").nav_file(3) end, desc = "Nav file 3" },
+    { "<leader>4", function() require("harpoon.ui").nav_file(4) end, desc = "Nav file 4" },
+  },
+}
