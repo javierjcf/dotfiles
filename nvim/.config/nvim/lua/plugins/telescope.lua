a3797b0b521fee72419666f391a221871e42004d
javierjcf <javier@comunitea.com>
2024-12-07 23:02:29 +0100

    [ADD]telescope: Live grep args

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,35 +1,56 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc="Find Files (root dir)" },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc="Live Grep" },
-        --git
-        { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "commits" },
-        { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "status" },
-        { "<leader>gf", "<cmd>Telescope git_files<cr>", desc = "files" },
-
-    },
-    opts = {
-        defaults = {
-            vimgrep_arguments = {
-                "rg",
-                "-L",
-                "--color=never",
-                "--no-heading",
-                "--with-filename",
-                "--line-number",
-                "--column",
-                "--smart-case",
-                "--hidden", -- buscar en archivos oculotos tb
-                "--no-ignore" -- No tener en cuenta .gitignore, .ignore. .rgignore
-            },
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.2",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-live-grep-args.nvim",
+  },
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files (root dir)" },
+    { "<leader>fg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", desc = "Grep (root dir)" },
+    -- git
+    { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "commits" },
+    { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "status" },
+    { "<leader>gf", "<cmd>Telescope git_files<cr>", desc = "files" },
+  },
+  opts = function(_, opts)
+    local lga_actions = require("telescope-live-grep-args.actions")
+    opts.defaults = {
+      vimgrep_arguments = {
+        "rg",
+        "-L",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden", -- Buscar en archivos ocultos
+        "--no-ignore", -- Ignorar .gitignore, .ignore, etc.
+      },
+      pickers = {
+        find_files = {
+          -- Mostrar archivos ocultos pero excluir el contenido de `.git/`
+          find_command = { "rg", "--files", "--hidden", "--no-ignore", "--glob", "!**/.git/*" },
         },
-        pickers = {
-		    find_files = {
-			    -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-			    find_command = { "rg", "--files", "--hidden",  "--no-ignore", "--glob", "!**/.git/*" },
-		    },
-	    },
-    },
+      },
+    }
+    opts.extensions = {
+      live_grep_args = {
+        auto_quoting = true, -- Habilitar/deshabilitar auto-cotizado
+        mappings = {
+          i = {
+            ["<C-k>"] = lga_actions.quote_prompt(),
+            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+          },
+        },
+      },
+    }
+  end,
+  config = function(_, opts)
+    local telescope = require("telescope")
+    telescope.setup(opts)
+    telescope.load_extension("live_grep_args")
+  end,
 }
+
